da68cb60031abbe0c8575827839d906f1d0373eb
user.email <sawepeter6@gmail.com>
2018-03-17 19:52:14 +0300

    intial commit

--- app/src/main/java/com/sawepeter/loan/MainActivity.java
+++ app/src/main/java/com/sawepeter/loan/MainActivity.java
@@ -2,12 +2,27 @@ package com.sawepeter.loan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private static final int MY_PERMISSION_STORAGE = 1;
+
+    TextView barcode;
+    Button scan;
+    ImageView codeimage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        barcode = (TextView)findViewById(R.id.textview);
+        scan = (Button)findViewById(R.id.button);
+        codeimage = (ImageView)findViewById(R.id.imageView);
     }
 }
