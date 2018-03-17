26bc82a41e0733bcc0956ad85b33401eae82a0a8
user.email <sawepeter6@gmail.com>
2018-03-17 20:25:50 +0300

    permission to read phone internal storage

--- app/src/main/java/com/sawepeter/loan/MainActivity.java
+++ app/src/main/java/com/sawepeter/loan/MainActivity.java
@@ -1,10 +1,17 @@
 package com.sawepeter.loan;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
@@ -23,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //request storage permission
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},MY_PERMISSION_STORAGE);
+        }
 
         barcode = (TextView)findViewById(R.id.textview);
         scan = (Button)findViewById(R.id.button);
@@ -34,6 +46,22 @@ public class MainActivity extends AppCompatActivity {
         if(!detector.isOperational()){
             barcode.setText("Could not set up detector");
         }
-      
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSION_STORAGE: {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
